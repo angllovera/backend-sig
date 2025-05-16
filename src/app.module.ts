4de48530ba74b4434e82acf7f298918f72baf000
Henry123JA <positronsito1.62@gmail.com>
2025-05-15 23:06:31 -0400

    incluido env_example y funcional

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/entities/user.entity';
 import { PagoModule } from './pago/pago.module';
-import { Pago } from './pago/entities/pago.entity'; 
+
+import { Pago } from './pago/entities/pago.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '8960406',
-      database: 'backend_sig',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Pago],
       synchronize: true,
     }),
